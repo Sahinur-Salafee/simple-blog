77e9d86fc7e758b8294c0326819f0ae52d2b713e
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-07 07:12:14 +0600

    File structured.

---
+++ controllers/single.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="blog-content">
+                <div class="blog-title">
+                    <h1>Blog Title</h1>
+                    <p>Blog descriptions...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/single.view.php
@@ -0,0 +1,5 @@
+<?php
+    require 'partials/header.php';
+    require './../controllers/single.php';
+    require 'partials/footer.php';
+?>

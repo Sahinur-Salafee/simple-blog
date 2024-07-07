751aa8b6b988321d50b5e6d78da172c73d8c943a
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-07 09:39:55 +0600

    Need to fix file require issue.

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php require 'views/partials/header.php' ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 col-lg-12 col-sm-12">
-            <h1>Here is All My Blogs</h1>
-            <ul>
-                <li>
-                    <a href="views/single.view.php">First Blog</a>
-                </li>
-                <li>
-                    <a href="views/single.view.php">Learn how can you learn about programming.</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div> 
+ <?php require 'views/index.view.php' ?>
 
 <?php require 'views/partials/footer.php' ?>
     

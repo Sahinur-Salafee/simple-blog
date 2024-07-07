53823ca48d9c789fbf4275480e7d40c61752ff2e
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-07 06:53:08 +0600

    file structured.

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Blog Project</title>
-    <link rel="stylesheet" href="assets/bootstrap.min.css">
-</head>
-<body>
+<?php require 'views/partials/header.php' ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-lg-12 col-sm-12">
+            <h1>This is a title</h1>
+        </div>
+    </div>
+</div> 
+
+<?php require 'views/partials/footer.php' ?>
     
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"></script>
-</body>
-</html>

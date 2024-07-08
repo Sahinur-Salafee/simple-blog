8deea1f528cd97bc57f6181397322b39ca0e6c43
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-08 07:01:18 +0600

    define CSS path on header

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Blog Project</title>
-    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH; ?>/bootstrap.min.css">
 </head>
 <body>

c9ee0d1a701fa66cb5ebf18c8206b64aabd80952
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-08 16:14:14 +0600

    Add title and styling blog create form.

--- views/create.view.php
+++ views/create.view.php
@@ -1,17 +1,20 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 col-lg-12 col-sm-12">
-            <form class="blog-create-form">
-                <div class="form-group">
-                    <label for="blogTitle">Blog Title</label>
-                    <input type="text" class="form-control" id="blogTitle" aria-describedby="blogTitle" placeholder="Blog Title">
-                </div>
-                <div class="form-group">
-                    <label for="blogDesc">Blog Description</label>
-                    <input type="text" class="form-control" id="blogDesc" aria-describedby="emailHelp" placeholder="Blog Description">
-                </div>
-                <button type="submit" class="btn btn-primary">Create Blog Post</button>
-            </form>
+<section class="create-blog-post-form">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12">
+                <h2>Create Blog Post</h2>
+                <form class="blog-create-form">
+                    <div class="form-group">
+                        <label for="blogTitle">Blog Title</label>
+                        <input type="text" class="form-control" id="blogTitle" aria-describedby="blogTitle" placeholder="Blog Title">
+                    </div>
+                    <div class="form-group">
+                        <label for="blogDesc">Blog Description</label>
+                        <input type="text" class="form-control" id="blogDesc" aria-describedby="emailHelp" placeholder="Blog Description">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Blog Post</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+</section>
